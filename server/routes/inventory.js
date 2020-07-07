0f5a833beeb317831a9c2f5328d6ba8cebb5fbe3
Omar Chaker <omarchaker@MacBook-Pro.local>
2020-07-07 18:26:56 -0400

    Backend get inventory item

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const inventoryData = require('../data/inventory.json');
+const inventoryData = require("../data/inventory.json");
+const { route } = require("./warehouse");
 
+//Get Single Inventory Item
+router.get("/:id", (req, res) => {
+	const found = inventoryData.some((item) => item.id === req.params.id);
 
-
-
+	if (found) {
+		res.json(inventoryData.filter((item) => item.id === req.params.id));
+	} else {
+		res.status(404).json({ msg: `No item with ID o ${req.params.id}` });
+	}
+});
 
 module.exports = router;
