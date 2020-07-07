64c2ebbbdc62aba04d234c88c20d63462040cc03
stephenkeddle <skeddle@bell.net>
2020-07-06 23:44:59 -0400

    started to create header - added to mixins, variables

---
+++ client/src/Components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+// import { Link } from 'react-router-dom';
+import Logo from '../../Assets/Logo/Logo-instock.svg';
+import '../Header/Header.scss';
+
+
+export default function Header() {
+  return (
+    <div className="nav">
+      <div className="nav__logo">
+
+        <img src={Logo} alt="instock logo" />
+
+
+      </div>
+      <div className="nav__container">
+        <ul className=" nav__menu">
+          <li className="nav__menu--text nav__menu--active">
+            <a href="">Inventory</a>
+          </li>
+          <li className="nav__menu--text nav__menu--notActive">
+            <a href="">Locations</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  )
+}
+
+
