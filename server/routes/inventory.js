e7c1af984319da0fa21cda3d2958009d010b8035
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-07-09 15:59:51 -0300

    Back-End added delete function for inventory. OLLLAUHOT-14

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -10,12 +10,29 @@ router.get("/:id", (req, res) => {
 	if (found) {
 		res.json(inventoryData.filter((item) => item.id === req.params.id));
 	} else {
-		res.status(404).json({ msg: `No item with ID o ${req.params.id}` });
+		res.status(404).json({ msg: `No item with ID: ${req.params.id}` });
 	}
 });
 
 //Get All Inventory Items
 
 router.get("/", (req, res) => res.json(inventoryData));
+
+//Delete Inventory Item
+
+router.delete("/:id", (req,res) => {
+	const found = inventoryData.some(item => item.id === req.params.id);
+	if (found) {
+		const newInventory = inventoryData.filter(item => item.id !== req.params.id);
+		res.json({
+			msg: `Deleted: ${req.params.id}`,
+			inventoryData: newInventory
+		})
+	} else {
+		res.status(404).json({
+			msg: `No item with ID: ${req.params.id}` 
+		})
+	}
+})
 
 module.exports = router;
