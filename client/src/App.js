1ceaf09020f92a5bd2ab57ef26e0b6d0a5ab6d58
Omar Chaker <omarchaker@MacBook-Pro.local>
2020-07-11 18:47:30 -0400

    front-end: add new warehouse modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,31 @@
-import React from 'react';
-import Header from '../src/Components/Header/Header'
-import Locations from '../src/Components/Locations/Locations'
-import axios from 'axios';
+import React from "react";
+import Header from "../src/Components/Header/Header";
+import Locations from "../src/Components/Locations/Locations";
+import axios from "axios";
 
-const urlW = 'http://localhost:8080'
+const urlW = "http://localhost:8080";
 
 class App extends React.Component {
-  state = {
-    locationWarehouse: []
-  }
+	state = {
+		locationWarehouse: [],
+	};
 
-  componentDidMount() {
-    axios.get(`${urlW}/warehouses`)
-      .then(res => {
-        const locationWarehouse = res.data;
-        this.setState({ locationWarehouse });
-        console.log(locationWarehouse)
-      })
+	componentDidMount() {
+		axios.get(`${urlW}/warehouses`).then((res) => {
+			const locationWarehouse = res.data;
+			this.setState({ locationWarehouse });
+			console.log(locationWarehouse);
+		});
+	}
 
-  }
-
-  render() {
-    return (
-
-      <div>
-        <Header />
-        <Locations warehouse={this.state.locationWarehouse} />
-      </div>
-
-    );
-  }
-
+	render() {
+		return (
+			<div>
+				<Header />
+				<Locations warehouse={this.state.locationWarehouse} />
+			</div>
+		);
+	}
 }
 
 export default App;
