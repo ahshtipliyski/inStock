6a22dd3de29d299408d4642c94e61b53f0196fe5
Omar Chaker <omarchaker@MacBook-Pro.local>
2020-07-12 16:42:44 -0400

    Front-end new warehouse post to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,93 @@
-import React from 'react';
-import Header from '../src/Components/Header/Header'
-import Locations from '../src/Components/Locations/Locations'
-import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import Inventory from '../src/Components/Inventory/Inventory';
-import Product from '../src/Components/Product/Product';
+import React from "react";
+import Header from "../src/Components/Header/Header";
+import Locations from "../src/Components/Locations/Locations";
+import axios from "axios";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from "react-router-dom";
+import Inventory from "../src/Components/Inventory/Inventory";
+import Product from "../src/Components/Product/Product";
 
-const urlW = 'http://localhost:8080'
+const urlW = "http://localhost:8080";
 class App extends React.Component {
-  state = {
-    locationWarehouse: [],
-    inventory: [],
-  }
+	state = {
+		locationWarehouse: [],
+		inventory: [],
+	};
 
+	initialMount() {
+		axios
+			.get(`${urlW}/warehouses`)
+			.then((res) => {
+				const locationWarehouse = res.data;
+				this.setState({ locationWarehouse });
+				// console.log(locationWarehouse)
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		axios
+			.get(`${urlW}/inventory`)
+			.then((res) => {
+				const inventory = res.data;
+				this.setState({ inventory });
+				// console.log(inventory)
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-  initialMount() {
-    axios.get(`${urlW}/warehouses`)
-      .then(res => {
-        const locationWarehouse = res.data;
-        this.setState({ locationWarehouse });
-        // console.log(locationWarehouse)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    axios.get(`${urlW}/inventory`)
-      .then(res => {
-        const inventory = res.data;
-        this.setState({ inventory });
-        // console.log(inventory)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
+	componentDidMount() {
+		this.initialMount();
+	}
 
-  componentDidMount() {
-    this.initialMount();
-  }
+	componentDidUpdate() {
+		console.log(this.props);
+	}
 
-  componentDidUpdate(){
-    console.log(this.props)
-  }
+	handleUpdateLocationWarehouses = (warehouses = []) => {
+		this.setState({
+			locationWarehouse: warehouses,
+		});
+	};
 
+	render() {
+		return (
+			<div>
+				<Router>
+					<Header />
+					<Switch>
+						<Redirect exact from='/' to='/warehouses' />
+						<Route exact path='/warehouses'>
+							<Locations
+								warehouse={this.state.locationWarehouse}
+								updateLocationWarehouses={this.handleUpdateLocationWarehouses}
+							/>
+						</Route>
+						<Route exact path='/inventory'>
+							<Inventory inventory={this.state.inventory} />
+						</Route>
 
-  render() {
-    return (
-      <div>
-        <Router>
-          <Header />
-          <Switch>
-            <Redirect exact from="/" to="/warehouses" />
-            <Route exact path="/warehouses">
-              <Locations warehouse={this.state.locationWarehouse} />
-            </Route>
-            <Route exact path="/inventory">
-              <Inventory inventory={this.state.inventory} />
-            </Route>
-       
-            {/* <Route path="/inventory/:id" >
+						{/* <Route path="/inventory/:id" >
               <Product inventory={this.state.inventory}/>
             </Route> */}
-            <Route path="/inventory/:id" render={(routeProps) => <Product warehouse={this.state.locationWarehouse} {...routeProps}/>}/>
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+						<Route
+							path='/inventory/:id'
+							render={(routeProps) => (
+								<Product
+									warehouse={this.state.locationWarehouse}
+									{...routeProps}
+								/>
+							)}
+						/>
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
 }
 export default App;
-
-
-
-
-
--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -1,52 +1,55 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const warehouseData = require('../data/warehouses.json');
-
+const warehouseData = require("../data/warehouses.json");
 
 //Get single warehouse
 router.get("/:id", (req, res) => {
-  const found = warehouseData.some((location) => location.id === req.params.id);
+	const found = warehouseData.some((location) => location.id === req.params.id);
 
-
-  if (found) {
-    res.json(warehouseData.filter((location) => location.id === req.params.id));
-  } else {
-    res.status(404).json({ err: `Warehouse id ${req.prams.id}` });
-  }
+	if (found) {
+		res.json(warehouseData.filter((location) => location.id === req.params.id));
+	} else {
+		res.status(404).json({ err: `Warehouse id ${req.prams.id}` });
+	}
 });
 
 // Get request for ALL warehouse
 
+router.get("/", (req, res) => {
+	res.json(warehouseData);
+});
 
-router.get('/', (req, res) => {
-  res.json(warehouseData);
-})
+// Post creating a new warehouse
 
-// Post creating a new warehouse 
-
-router.post('/', (req, res) => {
-  if (!req.body.name || !req.body.address.street || !req.body.address.location || !req.body.contact.name || !req.body.contact.position || !req.body.contact.phone || !req.body.contact.email) {
-    res.status(400).json({ message: 'Unable to POST' });
-  } else {
-    warehouseData.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      address: {
-        street: req.body.address.street,
-        location: req.body.address.location
-      },
-      contact: {
-        name: req.body.contact.name,
-        position: req.body.contact.position,
-        phone: req.body.contact.phone,
-        email: req.body.contact.email
-      },
-      inventoryCategories: req.body.inventoryCategories
-    })
-  }
-  res.json(warehouseData)
-})
+router.post("/", (req, res) => {
+	if (
+		!req.body.name ||
+		!req.body.address.street ||
+		!req.body.address.location ||
+		!req.body.contact.name ||
+		!req.body.contact.position ||
+		!req.body.contact.phone ||
+		!req.body.contact.email
+	) {
+		res.status(400).json({ message: "Unable to POST" });
+	} else {
+		warehouseData.push({
+			id: Date.now().toString(),
+			name: req.body.name,
+			address: {
+				street: req.body.address.street,
+				location: req.body.address.location,
+			},
+			contact: {
+				name: req.body.contact.name,
+				position: req.body.contact.position,
+				phone: req.body.contact.phone,
+				email: req.body.contact.email,
+			},
+			inventoryCategories: req.body.inventoryCategories,
+		});
+	}
+	res.json(warehouseData);
+});
 
 module.exports = router;
-
-
